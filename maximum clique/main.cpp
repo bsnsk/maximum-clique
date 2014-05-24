fa35d3731dfd71dde0dd4108ee0d8a9fe14bb410
Inability <inability@InabilitytekiMacBook-Pro.local>
2014-05-24 12:50:14 +0800

    Initial Commit

---
+++ maximum clique/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  maximum clique
+//
+//  Created by Inability on 14-5-24.
+//  Copyright (c) 2014年 Inability. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
